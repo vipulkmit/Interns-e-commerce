f7a36c6b4d291966e6f4a60d2241870a072012f7
vipulkmit <vipul.kmit@gmail.com>
2025-05-05 11:55:00 +0530

    working on profilepage ui

--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ProfileScreen from "../screens/Profile/ProfileScreen";
 
+const Stack = createNativeStackNavigator();
 const ProfileNavigator = () => {
   return (
-    <View>
-      <Text>ProfileNavigator</Text>
-    </View>
-  )
-}
+    <Stack.Navigator
+      // @ts-ignore: Suppress TypeScript error for 'id'
+      id="ProfileNavigator"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
+};
 
-export default ProfileNavigator
+export default ProfileNavigator;
---
+++ src/screens/Profile/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const ProfileScreen = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default ProfileScreen;
