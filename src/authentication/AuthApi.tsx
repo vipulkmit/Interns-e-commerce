0e4e17e5dc21842d9ebd54b14e7a88908bc038ca
vipulkmit <vipul.kmit@gmail.com>
2025-05-22 12:32:27 +0530

    Password change error fix

--- src/authentication/AuthApi.tsx
+++ src/authentication/AuthApi.tsx
@@ -8,6 +8,8 @@ export const loginUser = (data: any) =>
   axiosInstance.post(ENDPOINTS.LOGIN, data);
 export const forgetpassword = (data: any) =>
   axiosInstance.post(ENDPOINTS.FORGET_PASSWORD, data);
+export const changepassword = (data: any) =>
+  axiosInstance.post(ENDPOINTS.CHANGE_PASSWORD, data);
 export const verifyotp = (data: any) =>
   axiosInstance.post(ENDPOINTS.VERIFY_OTP, data);
 export const googlelogin = (data: any) =>
